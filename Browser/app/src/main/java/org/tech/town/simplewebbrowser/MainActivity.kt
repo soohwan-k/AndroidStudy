9ee787167f3a0148f97b5707fe005278ed5647b5
soohwan-k <newpoo@naver.com>
2023-03-21 20:10:41 +0900

    feat: navigation

--- Browser/app/src/main/java/org/tech/town/simplewebbrowser/MainActivity.kt
+++ Browser/app/src/main/java/org/tech/town/simplewebbrowser/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +18,18 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.webView)
     }
 
+    private val homeButton: ImageButton by lazy {
+        findViewById(R.id.homeButton)
+    }
+
+    private val backButton: ImageButton by lazy {
+        findViewById(R.id.backButton)
+    }
+
+    private val forwardButton: ImageButton by lazy {
+        findViewById(R.id.forwardButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,22 +38,46 @@ class MainActivity : AppCompatActivity() {
         bindViews()
     }
 
+    override fun onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews() {
-        webView.apply{
+        webView.apply {
             webViewClient = WebViewClient()
             settings.javaScriptEnabled = true
-            loadUrl("http://www.google.com")
+            loadUrl(DEFAULT_URL)
         }
     }
 
     private fun bindViews() {
         addressBar.setOnEditorActionListener { textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl("http://"+addressBar.text.toString())
+                webView.loadUrl("http://" + addressBar.text.toString())
             }
 
             return@setOnEditorActionListener false
         }
+
+        backButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        forwardButton.setOnClickListener {
+            webView.goForward()
+        }
+
+        homeButton.setOnClickListener(){
+            webView.loadUrl(DEFAULT_URL)
+        }
+    }
+
+    companion object{
+        private const val DEFAULT_URL = "http://www.google.com"
     }
 }
