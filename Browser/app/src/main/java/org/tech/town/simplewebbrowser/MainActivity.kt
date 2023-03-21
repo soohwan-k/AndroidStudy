6b767d4aea651f082f7a54a6b989e8de9936b3b2
soohwan-k <newpoo@naver.com>
2023-03-21 19:29:46 +0900

    feat: Init WebView

--- Browser/app/src/main/java/org/tech/town/simplewebbrowser/MainActivity.kt
+++ Browser/app/src/main/java/org/tech/town/simplewebbrowser/MainActivity.kt
@@ -1,11 +1,46 @@
 package org.tech.town.simplewebbrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply{
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl("http://"+addressBar.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
     }
 }
